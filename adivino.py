66a0c2af89a3f29cc6e9912fc46b16572a5e1b7d
TheMordekancer <102340327+TheMordekancer@users.noreply.github.com>
2022-03-25 14:54:18 -0300

    Update adivino.py

--- adivino.py
+++ adivino.py
@@ -4,10 +4,10 @@ import random
 numero_aleatorio = random.randrange(101)
 gane = False
 print("Tenés 5 intentos para adivinar un numero entre 0 y 100")
-intento = 1
-while intento < 6 and not gane:
-    numeroIngresado = int(input('Ingresa tu número: '))
-    if numeroIngresado == numero_aleatorio:
+intento = 0
+while intento < 5 and not gane:
+    numero_ingresado = int(input('Ingresa tu número: '))
+    if numero_ingresado == numero_aleatorio:
         print('Ganaste! y necesitaste {} intentos!!!'.format(intento))
         gane = True
     else:
